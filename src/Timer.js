fde6ce57daa5eb71f93b5beba95f3a441bfcbced
Ahmed <ahmedali.96@hotmail.com>
2021-04-17 17:03:59 -0400

    updated timer functionality

--- src/Timer.js
+++ src/Timer.js
@@ -1,34 +1,34 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
-const Timer = ({gameCount, setTimer}) => {
-    const [minutes,setMinutes] = useState(0);
-    const [seconds,setSeconds]= useState(0);
-    let time = 0;
-    
-    useEffect(() => {
-        startTimer();
-      },[]);
+const Timer = ({ gameCount, setTimer }) => {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  let time = 0;
+  let interval;
 
+  useEffect(() => {
+    startTimer();
 
+    return () => {
+      clearInterval(interval);
+      setTimer(time);
+    };
+  }, []);
 
-    const startTimer = () =>{
-        //timer that updates the value of minutes and seconds every second
-        let interval = setInterval ( () => {
-            if (gameCount >= 11){
-                clearInterval(interval);
-                
-            } else {
-                time = time+1
-                setTimer(time);
-                setMinutes(Math.floor(time / 60))
-                setSeconds(Math.floor(time % 60))        
-            }
-        }, 1000)
-    }
+  const startTimer = () => {
+    //timer that updates the value of minutes and seconds every second
+    interval = setInterval(() => {
+      time = time + 1;
+      setMinutes(Math.floor(time / 60));
+      setSeconds(Math.floor(time % 60));
+    }, 1000);
+  };
 
-    return(
-        <h2>{minutes} : { seconds < 10 ? `0${ seconds }` : seconds }</h2>
-    )
-}
+  return (
+    <h2>
+      {minutes} : {seconds < 10 ? `0${seconds}` : seconds}
+    </h2>
+  );
+};
 
-export default Timer
+export default Timer;
