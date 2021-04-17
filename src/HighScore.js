56b4f6627526b81b1732a5469a49cf6e0fbb7d60
Ahmed <ahmedali.96@hotmail.com>
2021-04-17 18:03:22 -0400

    updated highscore.js

--- src/HighScore.js
+++ src/HighScore.js
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
 import firebase from "./firebase";
 
 const HighScore = () => {
+  const [highScores, setHighScores] = useState([]);
+
+  useEffect(() => {
+    const dbRef = firebase.database().ref();
+
+    dbRef.on("value", (res) => {
+      const data = res.val();
+      const newState = [];
+
+      for (let key in data) {
+        newState.push({
+          key: key,
+          ...data[key],
+          // comments: [],
+        });
+      }
+
+      setHighScores(newState);
+    });
+  }, []);
+  console.log(highScores);
   return (
     <>
       <h1>HighScore</h1>
+
+      <ul>
+        {highScores.map((highscore, index) => {
+          const { name, score, time } = highscore;
+          return (
+            <li key={index}>
+              <span>{name}:</span>
+              <span>{score}</span>
+              <span>{time}</span>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
