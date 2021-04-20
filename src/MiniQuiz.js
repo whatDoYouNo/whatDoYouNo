e4ff61a6a766049638f8d1e2e389bf61e108463f
Ahmed <ahmedali.96@hotmail.com>
2021-04-20 17:30:51 -0400

    added wrong class

--- src/MiniQuiz.js
+++ src/MiniQuiz.js
@@ -1,6 +1,5 @@
-import LoadingWheel from './LoadingWheel';
-import {useState} from 'react';
-
+import LoadingWheel from "./LoadingWheel";
+import { useState } from "react";
 
 const MiniQuiz = ({
   gameCount,
@@ -9,55 +8,61 @@ const MiniQuiz = ({
   setGamePoints,
   gameData,
 }) => {
-
-  const [miniQuizClass, setMiniQuizClass] = useState('miniQuiz')
+  const [miniQuizClass, setMiniQuizClass] = useState("miniQuiz");
 
   const randomBool = Boolean(Math.round(Math.random()));
-  const handleClick = () => {
+  const handleCorrect = () => {
     setGameCount(gameCount + 1);
     setGamePoints(gamePoints + 1);
-    setMiniQuizClass('miniQuiz correct')
-    setTimeout( () => {
-      setMiniQuizClass('miniQuiz')
-    },250)
+    setMiniQuizClass("miniQuiz correct");
+    setTimeout(() => {
+      setMiniQuizClass("miniQuiz");
+    }, 250);
+  };
+
+  const handleIncorrect = () => {
+    setMiniQuizClass("miniQuiz incorrect");
+
+    setGameCount(gameCount + 1);
+
+    setTimeout(() => {
+      setMiniQuizClass("miniQuiz");
+    }, 250);
   };
 
   return (
     <div className="quizContainer">
       <section className={miniQuizClass}>
-        {
-          gameData.definition === ""
-            ? <LoadingWheel />
-            : <>
-              <p className="definition">{gameData.definition.slice(gameData.definition.indexOf("\t"))}</p>
-              {/*<button onClick={()=>setGameCount(gameCount+1)}>{gameData.answer}</button>*/}
+        {gameData.definition === "" ? (
+          <LoadingWheel />
+        ) : (
+          <>
+            <p className="definition">
+              {gameData.definition.slice(gameData.definition.indexOf("\t"))}
+            </p>
+            {/*<button onClick={()=>setGameCount(gameCount+1)}>{gameData.answer}</button>*/}
 
-              {randomBool ? (
-                <button onClick={handleClick}>{gameData.answer}</button>
-              ) : (
-                <button onClick={() => setGameCount(gameCount + 1)}>
-                  {gameData.wrong}
-                </button>
-              )}
+            {randomBool ? (
+              <button onClick={handleCorrect}>{gameData.answer}</button>
+            ) : (
+              <button onClick={handleIncorrect}>{gameData.wrong}</button>
+            )}
 
-              {randomBool ? (
-                <button onClick={() => setGameCount(gameCount + 1)}>
-                  {gameData.wrong}
-                </button>
-              ) : (
-                <button onClick={handleClick}>{gameData.answer}</button>
-              )}
-            </>
-        }
-      {/* Initial boolean randomizer function that will give us a value of true or false*/}
-      {/* Check if the the value is true?*/}
-      {/* If so print answer to 1st button */}
-      {/* If not print the wrong answer to the 1st button*/}
-      {/* Print whichever remains to the second button depending on if the first button got the correct answer (was true)*/}
+            {randomBool ? (
+              <button onClick={handleIncorrect}>{gameData.wrong}</button>
+            ) : (
+              <button onClick={handleCorrect}>{gameData.answer}</button>
+            )}
+          </>
+        )}
+        {/* Initial boolean randomizer function that will give us a value of true or false*/}
+        {/* Check if the the value is true?*/}
+        {/* If so print answer to 1st button */}
+        {/* If not print the wrong answer to the 1st button*/}
+        {/* Print whichever remains to the second button depending on if the first button got the correct answer (was true)*/}
       </section>
-    </div> 
+    </div>
   );
-
 };
 
 export default MiniQuiz;
