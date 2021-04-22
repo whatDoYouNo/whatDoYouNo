21355f5ae685518a2f237cb4eeeed418a86e8b2f
Ahmed <ahmedali.96@hotmail.com>
2021-04-22 15:03:35 -0400

    added react-confetti + tab update @ SideBar

--- src/SideBar.js
+++ src/SideBar.js
@@ -2,42 +2,44 @@ import HighScore from "./HighScore";
 import { useState } from "react";
 
 const SideBar = () => {
-    const [sideBarActive, setSideBarActive] = useState(false);
-    const [nameOfClass, setNameOfClass] = useState('leaderBoard')
+  const [sideBarActive, setSideBarActive] = useState(false);
+  const [nameOfClass, setNameOfClass] = useState("leaderBoard");
 
-    const handleClick = () => {
-        setSideBarActive(!sideBarActive);
-        if (!sideBarActive) {
-            setNameOfClass('leaderBoard active')
-        } else {
-            setNameOfClass('leaderBoard')
-        }
-
+  const handleClick = () => {
+    setSideBarActive(!sideBarActive);
+    if (!sideBarActive) {
+      setNameOfClass("leaderBoard active");
+    } else {
+      setNameOfClass("leaderBoard");
     }
+  };
 
-    return (
-        <>
-            <aside className="drawer" onClick={handleClick}>
-                <h2 className="vertical">
-                    <span>L</span>
-                    <span>e</span>
-                    <span>a</span>
-                    <span>d</span>
-                    <span>e</span>
-                    <span>r</span> 
-                    <span>B</span>
-                    <span>o</span>
-                    <span>a</span>
-                    <span>r</span>
-                    <span>d</span>
-                    <span>&lt;&lt;</span>
-                </h2>
-                <HighScore nameOfClass={nameOfClass} />
-            </aside>
-        </>
+  return (
+    <>
+      <aside
+        className="drawer"
+        tabIndex="0"
+        onClick={handleClick}
+        onFocus={handleClick}
+      >
+        <h2 className="vertical">
+          <span>L</span>
+          <span>e</span>
+          <span>a</span>
+          <span>d</span>
+          <span>e</span>
+          <span>r</span>
+          <span>B</span>
+          <span>o</span>
+          <span>a</span>
+          <span>r</span>
+          <span>d</span>
+          <span>&lt;&lt;</span>
+        </h2>
+        <HighScore nameOfClass={nameOfClass} />
+      </aside>
+    </>
+  );
+};
 
-
-    )
-}
-
-export default SideBar
+export default SideBar;
